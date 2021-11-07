1d349354140da5a787108dab7202c95f1581e99d
Stephen Wuh <swuh87@gmail.com>
2021-11-07 18:12:54 -0500

    dialog box project video embeded

--- src/components/ProjectsDialog.js
+++ src/components/ProjectsDialog.js
@@ -4,38 +4,83 @@ import PropTypes from "prop-types";
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
 
-
-
 export default function ProjectsDialog(props) {
-    const { onClose, open, details} = props;
-  
-    const handleClose = () => {
-      onClose();
-    };
-  
-    return (
-      
-        <Dialog onClose={handleClose} open={open} maxWidth='lg'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col'>
-                <DialogTitle className='text-center'>{details.title}</DialogTitle>
-              </div>
-            </div>
-            
-            <div className='row'>
-              <div className='col-4'>
-                <img src={details.image[0] } alt='image' className='img-fluid'/>
-              </div>
-            </div>
-            <p>{details.details}</p>
-          </div>
-        </Dialog>
-      
-    );
-  }
-  
-  ProjectsDialog.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    open: PropTypes.bool.isRequired,
+  const { onClose, open, details } = props;
+
+  const handleClose = () => {
+    onClose();
   };
+
+  const newDetails = details.details.split("\n").map((str) => <li>{str}</li>);
+
+  // const images = details.image.map()
+
+  return (
+    <Dialog onClose={handleClose} open={open} maxWidth="md">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <DialogTitle className="text-center">{details.title}</DialogTitle>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            {details.video !== "" && 
+              <>
+                <div style={{padding:'50.42% 0 0 0', position: 'relative', border: '1px solid #D3D3D3', boxShadow: '2px 2px #D3D3D3'}}>
+                  <iframe
+                    src={details.video}
+                    frameborder="0"
+                    allow="autoplay; fullscreen; picture-in-picture"
+                    allowfullscreen
+                    style={{position: 'absolute', top:'0', left:'0', width:'100%',height:'100%'}}
+                  ></iframe>
+                </div>
+                <script src="https://player.vimeo.com/api/player.js"></script>
+              </>
+            }
+            {details.video === "" &&
+            <img src={details.image[0]} alt="image" className="img-fluid" />
+            }
+            
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h5>Tools Used</h5>
+            <ul>
+              <li>{details.tech}</li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h5>Description</h5>
+            <ul>{newDetails}</ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h5>Source Code</h5>
+            <ul>
+              <li>
+                <a
+                  href={details.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {details.github}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
+
+ProjectsDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+};
--- src/components/projectInfo.js
+++ src/components/projectInfo.js
@@ -11,26 +11,32 @@ import td1 from ".././images/project/stickyNote/td1.PNG";
 const projectInfo = [
   {
     image: [dc1, dc2, dc3, dc4, dc5],
+    video: "https://player.vimeo.com/video/639334937?h=5ce8d67c41&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
     title: "Digital Closet",
     description: "Full stack app that helps users dress stylishly.",
-    details: "",
+    details: "The main goal of Digital Closet is to help users dress stylishly. It aims to do so by allowing users to catalogue and organize their clothes digitally and by providing them with outfit recommendations based on the clothes they have in their digital closet. \n This app is a result of a frustration I had while living in South Korea. ",
     github: "https://github.com/stephwuh/digitalCloset",
+    tech: "React, Redux, Cloudinary, Node.js, Express, Bcrypt, Sequelize ORM, PostgreSQL"
   },
   {
     image: [jat1, jat2],
+    video: "",
     title: "Job Application Tracker",
     description: "Allows users to track their job applications",
     details: "",
     github: "https://github.com/stephwuh/khaki-apps",
+    tech: "React, Material UI, Bootstrap, Jest, React Testing Library, JSON Server"
   },
   {
     image: [td1],
+    video: "",
     title: "Stick Note To-do List",
     description:
       "Users are able to create, save, and delete to-do items in the form of stick notes",
     details:
       "I wanted to make a to-list app that was a bit more visually appealing",
     github: "https://github.com/stephwuh/Sticky-Note-Todo-List",
+    tech: "JavaScript, Node.js, Express"
   },
 ];
 
